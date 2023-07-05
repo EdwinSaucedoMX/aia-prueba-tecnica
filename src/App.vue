31412fda28c9a9c12eca2268e176587defc351b3
Edwin Saucedo <edwin-saucedo@hotmail.com>
2023-07-05 09:33:49 -0600

    Project semifinished

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import Card from "./components/Card.vue";
+
+let data = ref([]);
+
+onMounted(() => {
+	fetch("/proyectos.json")
+		.then((res) => {
+			return res.json();
+		})
+		.then((result) => {
+			data.value = result;
+			data.value.sort((a: any, b: any) =>
+				a.isAvailable < b.isAvailable ? 1 : -1
+			);
+
+			console.log(result);
+		});
+});
+</script>
+
+<template>
+  <h1 className="title">Nuestros Proyectos</h1>
+  <main className="card-container">
+		<div
+			className="grid-container"
+			v-for="{
+				name,
+				description,
+				location,
+				totalArea,
+				img,
+				terrace,
+				rooms,
+				isAvailable,
+			} in data"
+		>
+			<Card
+				v-bind:name="name"
+				v-bind:description="description"
+				v-bind:location="location"
+				v-bind:totalArea="totalArea"
+				v-bind:img="img"
+				v-bind:terrace="terrace"
+				v-bind:rooms="rooms"
+				v-bind:isAvailable="isAvailable"
+			/>
+		</div>
+
+    
+	</main>
+</template>
---
+++ src/components/Card.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+
+defineProps({
+	name: {
+		default: "Nombre no registrado",
+		type: String,
+	},
+	description: {
+        default: "Sin descripción",
+        type: String,
+    },
+	location: {
+        default: "Ubicación no registrada",
+        type:String,
+    },
+	totalArea: {
+        default: -1,
+        type: Number,},
+	img: {
+        default: "/no-image.png",
+        type: String,
+    },
+	terrace: {
+        default: -1,
+        type: Number,
+    },
+	rooms: {
+        default: -1,
+        type: Number,
+    },
+	isAvailable: {
+        default: false,
+        type: Boolean,
+    },
+});
+</script>
+
+<template>
+    <figure class="card">
+        <h3>{{ name }}</h3>
+        <p>{{ description }}</p>
+        <h4>{{ location }}</h4>
+        <span
+            ><p>
+                {{ `Área Total: ` }}
+                {{ totalArea > 0 ? `${totalArea} m2` : "No registrada"}}
+            </p>
+            <p>
+                {{ `Terraza: ` }}
+                {{ terrace > 0 ? `${terrace} m2` : "No" }}
+            </p>
+        </span>
+        <h5 class="available" v-if="isAvailable">Disponible</h5>
+        <h5 class="unavailable" v-else>No Disponible</h5>
+        <img v-bind:src="img">
+    </figure>
+	
+
+	
+</template>
+
